a15173985ca1a2029d5bc5c07f561314664d138f
Qypol342 <Qypol342@gmail.com>
2022-01-30 12:13:16 +0100

    cahnge regsrtiation to sw

--- sw.js
+++ sw.js
@@ -1,3 +1,4 @@
 self.addEventListener('push', () => {
-  self.registration.showNotification('Hello world!', {});
+  //self.registration.showNotification('Hello world!', {});
+  self.sw.showNotification('Notifiaction working', {});
 });
